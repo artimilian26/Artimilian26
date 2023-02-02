88439dce678950cf4080d2c4468bd528c7ecc0c9
Artimilian26 <Artimilian26@mail.ru>
2023-02-02 19:50:33 +0300

    почти все

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 let temp =-15;
 if (temp<-30) {
     console.log("Оставайтесь дома");
-} else 1f (temp >= -30 8& temp < -10); 
+} else if (temp >=-30&&temp< -10) {
+    console.log("Сегодня холодно"); 
+} else if (temp >=-10&&temp<5) {
+    console.log ("Не холодно");
+} else if (temp >=- 5&&temp<15) {
+    console.log ("Тепло") 
+} 
