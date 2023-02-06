c1ee6983cbc37b7f7da0e58642c741c073652855
Artimilian26 <Artimilian26@mail.ru>
2023-02-06 20:53:21 +0300

    помогите я не знаю как делать

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-let temp =-15;
-if (temp<-30) {
-    console.log("Оставайтесь дома");
-} else if (temp >=-30&&temp< -10) {
-    console.log("Сегодня холодно"); 
-} else if (temp >=-10&&temp<5) {
-    console.log ("Не холодно");
-} else if (temp >=- 5&&temp<15) {
-    console.log ("Тепло") 
-} 
+// 2 Если переменная a больше нуля, то выведите 'Верно', иначе выведите 'Неверно'.
+
+a > 0 ;
+
+
