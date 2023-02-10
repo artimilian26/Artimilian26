8ffb0d67be9d0b657420ddfd824260cf97b02726
Artimilian26 <Artimilian26@mail.ru>
2023-02-10 10:20:06 +0300

    классная работа 2

---
+++ lesson2.js
@@ -0,0 +1,24 @@
+//let str ="мама мыла раму"
+//for (let i=0; i < 5; i++) {
+//console.log (str) 
+//}
+
+// вывести числа от 1 до 40
+//for (i = 1; i<= 40; i++) {
+//    console.log (i);
+//}
+
+//for (i =-1; i>=-40; i--) {
+//    console.log (i)
+//}
+//for (i =-1; i>=-40; i--) {
+//    if (i%2===0){
+//        console.log (i)
+//    }
+//}
+
+let sum =0;
+for (i=1; i<=100; i++){
+    sum = sum + i
+}
+console.log (sum)
