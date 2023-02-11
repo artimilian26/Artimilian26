7d4f46de1cfced018666e5366a1d7880c3fcfb8b
Artimilian26 <Artimilian26@mail.ru>
2023-02-11 09:52:07 +0300

    /

--- experiments.js
+++ experiments.js
@@ -1,4 +1,8 @@
-let str= "фраза"
-for (i = 0; i < 1000; i++) {
-    console.log (str)
-}
+//for (i =-1;i >= -10; i--) 
+//    console.log (i)
+
+//for (i =-1;i >= -10; i--) {
+//    if (i%2===0)  {
+//        console.log (i) 
+//    }
+//}
