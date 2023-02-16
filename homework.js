819d9976416321cb998c98439793271d681084db
Artimilian26 <Artimilian26@mail.ru>
2023-02-16 18:43:35 +0300

    16,02

--- homework.js
+++ homework.js
@@ -1,1 +1,8 @@
-let str = "234432"
+let str = "наворован";
+let count =0;
+for (i=0; i < str.length/2; i++ ) {
+    if (str[i] == str[str.length -1 -i ]) {
+    count = count + 1; }
+}
+if (count>=str.length/2) {
+    console.log("Паллиндром"); }
