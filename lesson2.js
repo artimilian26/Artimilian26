4311986d3e104cc69626976f63ba62abfadc3a17
Artimilian26 <Artimilian26@mail.ru>
2023-03-07 22:33:07 +0300

    Год прошел каак сон пустой

--- lesson2.js
+++ lesson2.js
@@ -17,8 +17,69 @@
 //    }
 //}
 
-let sum =0;
-for (i=1; i<=100; i++){
-    sum = sum + i
+//let str1 = "мама";
+//let str2 = "папа";
+//let count = 0;
+//for (i = 0; i <str1.length; i++) {
+//    if(str1[i]!=str2[i]){
+//        count ++;
+//    }
+//}
+//if (count==0){
+//    console.log("слова равны");
+//} else console.log (count)
+//let str = "Хламидомонада";
+//for (i = 0; i < str.length; i++) {
+//    console.log (str[i])
+//}
+
+//let str= "875621";
+//let sum = 0;
+//for(i=0 ; i < str.length;i++){
+//if (str[i] % 2!=0 ) {
+//    sum = sum + +str[i];
+//  }
+//}
+//console.log (sum)
+
+//let str2 = "2222"
+//let str1 = "1111"
+//let sum1 = 0;
+//let sum2 =0;
+//for(i=0 ; i < str1.length;i++){
+//    sum1 =sum1 + +str1[i];
+//}
+//for(i=0 ; i < str2.length;i++){
+//    sum2 =sum2 + +str2[i];
+//}
+//if (sum1>sum2){console.log("сумма 1 больше");
+//} else if 
+
+//function sum(num1,num2) {
+//    return num1 + num2;
+//}
+//console.log(sum(1,2));
+
+//let str = "FF11FF";
+//let sum = 0;
+//let countF = 0;
+// for(i=0;i < str.lenght;i++) {
+//  if (str[i] == "F") {
+//    console.log("Победа");
+ // } 
+
+ //let str1 = "Abc"
+//let str2 =" poi"
+// let str3 =" hhjs"
+// str4 = str1[0] + str2[1] +str3[2]
+// console.log(str4);
+
+let str1 = "Капуста";
+let str2 ="Морковь";
+let str3= "Свветана";
+let str4="";
+for(i=0;i < str1.length;i++) {
+    str4 = str1[i] + str2[i] +str3[i];
+    console.log (str4)
 }
-console.log (sum)
+
